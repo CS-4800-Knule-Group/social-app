18a88698ce28ca4948c1266f85f1acff9d3f99ad
Nathanael Garcia <nathanaelg2099@gmail.com>
2024-09-26 13:00:15 -0700

    Began profile page, added placeholder text for username, name, bio, and profile pic

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,40 +1,21 @@
 import React from 'react'
-import logo from '/Knulle-Logo.png';
 import { Link } from 'react-router-dom'
 import './Header.css';
 
 
 const Header = () => {
   return (
-    <><div>
-        <Link to={'/'}>
-            <img src={logo} alt='K[NULL]E' className='logo'/>
-        </Link>
+    <header class='header'>
+        <div class="header-container">
+        <img src="Knulle-Logo.png" alt="Logo" class="logo"/>
+            <nav class="top-links">
+            <Link to="/" class="nav-link">Home</Link>
+            <Link to="/data" class="nav-link">Data</Link>
+            <Link to="/profile" class="nav-link">Profile</Link>
+            <Link to="/login" class="nav-link">Register</Link>
+            </nav>
         </div>
-        <div>
-        <header className='header'>
-            <div className='header-container'>
-                <div className='head-left'>
-                    <Link to={'/'}>
-                        <a className='head-text'>Home</a>
-                    </Link>
-                </div>
-                <div className='head-right'>
-                    <Link to={'/data'}>
-                        <a className='head-text'>Data</a>
-                    </Link>
-                    
-                    <Link to={'/profile'}>
-                        <a className='head-text'>Profile</a>
-                    </Link>
-                    <Link to={'/login'}>
-                        <a className='head-text'>Login</a>
-                    </Link>
-                </div>
-            </div>
-        </header>
-    </div>
-    </>
+    </header>
   )
 }
 
--- src/Pages/profile.jsx
+++ src/Pages/profile.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import './Profile.css'
 
 const Profile = () => {
   return (
-    <div>profile</div>
+    <div>
+      <div className='profile'>
+        <img className= 'profilePic' src='/kirb.jpg' height={100} width={100} />  
+        <div className='profile-text'>
+          <h1 className='username'>This is the username</h1>
+          <h3 className='fullName'>This is the real name</h3>
+        </div>
+        <p className='bio-head'><b>Bio</b></p>
+        <p className='bio'>
+          This is your bio <br/>
+          I am just gonna keep making <br/>
+          Lines. I have no fuckin idea how to make this auto align <br/>
+          so... lmao. Lets try a really long string 12345 123135426 1234151 12341512 123412341 12341234 12341234 1234124 fuck we gotta make this string really long aaah aaaha aaaaha aaahha aaaaha aaaah aaaah<br/>
+          Lets go it already auto aligns I've just been wasting my time lmao
+        </p>
+      </div>
+    </div>
   )
 }
 

a30cc45b1c89ea3f1edbee6984a9a8d1ef7fafe4
Otwo06 <46037767+Otwo6@users.noreply.github.com>
2024-09-26 20:17:55 -0700

    Register Page UI Update

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,33 +1,38 @@
 import React from 'react'
+import ad from '/RocketLeagueAd.jpg';
+import logo from '/Knulle-Logo.png';
+import './register.css';
 
 const Login = () => {
-  return (
-    <div>
-        <div>  
-            <h4>Welcome to K[NULL]E!</h4>
-            <p>To get started, we need some information about you</p>
-            <br/>
-            <form action='https://knule.duckdns.org/users/newUser' method='post'>
-                <label for='fname'>First Name:</label>
-                <input type='text' id='fname' name='fname'/>
-                <br/>
-                <label for='lname'>Last Name: </label>
-                <input type='text' id='lname' name='lname'/>
-                <br/>
-                <label for='username'>Username: </label>
-                <input type='text' id='username' name='username'/>
-                <br/>
-                <label for='email'>Email: </label>
-                <input type='email' id='email' name='email'/>
-                <br/>
-                <label for='password'>Password: </label>
-                <input type='password' id='password' name='password'/>
-                <br/>
-                <input class='submitButton' type='submit'/>
-            </form>
-        </div>
-    </div>
-  )
+return (
+<div>
+	<div class="columns">
+		<div class="registerInfo">
+			<img src="Knulle-Logo.png" alt="Logo" class="logo"/>
+			<h4 class="title">Create an Account</h4>
+			<p class="subtitle">To get started, we need some information about you.</p>
+			<br/>
+			<form action='https://knule.duckdns.org/users/newUser' method='post'>
+				<input type='text' id='fname' name='fname' placeholder="First Name" class="textBox"/>
+				<br/>
+				<input type='text' id='lname' name='lname' placeholder="Last Name" class="textBox"/>
+				<br/>
+				<input type='text' id='username' name='username' placeholder="Username" class="textBox"/>
+				<br/>
+				<input type='email' id='email' name='email' placeholder="Email" class="textBox"/>
+				<br/>
+				<input type='password' id='password' name='password' placeholder="Password" class="textBox"/>
+				<br/>
+				<input class='submitButton' type='submit'/>
+			</form>
+		</div>
+
+		<div class="image">
+			<img src="RocketLeagueAd.jpg" alt="Image"/>
+		</div>
+	</div>
+</div>
+)
 }
 
 export default Login

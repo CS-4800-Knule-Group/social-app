fb93a70879345de891451128fa21917fe99bfc58
Nathanael Garcia <nathanael.garcia@sjsu.edu>
2024-10-03 18:29:34 -0700

    Changed 'class' to 'classname'

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -4,28 +4,28 @@ import './Register.css';
 const Register = () => {
 return (
 <div>
-	<div class="columns">
-		<div class="registerInfo">
+	<div className="columns">
+		<div className="registerInfo">
 			<img className= 'logo' src='/Knulle-Logo.png'/>  
-			<h4 class="title">Create an Account</h4>
-			<p class="subtitle">To get started, we need some information about you.</p>
+			<h4 className="title">Create an Account</h4>
+			<p className="subtitle">To get started, we need some information about you.</p>
 			<br/>
 			<form action='https://knule.duckdns.org/users/newUser' method='post'>
-				<input type='text' id='fname' name='fname' placeholder="First Name" class="textBox"/>
+				<input type='text' id='fname' name='fname' placeholder="First Name" className="textBox"/>
 				<br/>
-				<input type='text' id='lname' name='lname' placeholder="Last Name" class="textBox"/>
+				<input type='text' id='lname' name='lname' placeholder="Last Name" className="textBox"/>
 				<br/>
-				<input type='text' id='username' name='username' placeholder="Username" class="textBox"/>
+				<input type='text' id='username' name='username' placeholder="Username" className="textBox"/>
 				<br/>
-				<input type='email' id='email' name='email' placeholder="Email" class="textBox"/>
+				<input type='email' id='email' name='email' placeholder="Email" className="textBox"/>
 				<br/>
-				<input type='password' id='password' name='password' placeholder="Password" class="textBox"/>
+				<input type='password' id='password' name='password' placeholder="Password" className="textBox"/>
 				<br/>
-				<input class='submitButton' type='submit'/>
+				<input className='submitButton' type='submit'/>
 			</form>
 		</div>
 
-		<div class="image">
+		<div className="image">
 			<img src='/RocketLeagueAd.jpg'/>
 		</div>
 	</div>

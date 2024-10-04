8257e833c803334bb861e3544545c873b9ef25c9
Nathanael Garcia <nathanael.garcia@sjsu.edu>
2024-10-03 17:49:14 -0700

    Added logic to feed page & updated profile cookies

--- src/Pages/feed.jsx
+++ src/Pages/feed.jsx
@@ -1,20 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Feed.css'
 
 const Feed = () => {
+	
+	const[posts, setPosts] = useState([])
+	const apiPosts = 'https://knule.duckdns.org/posts'
+
+	useEffect(() => {
+		
+		const fetchPosts = async () => {
+			try {
+				const response = await fetch(apiPosts, {
+				method: 'GET', // should be lowercase 'method'
+				});
+		
+				if (!response.ok) {
+				throw new Error('Could not reach /posts');
+				}
+				const postsData = await response.json();
+				setPosts(postsData); // Update the state with the fetched users
+				console.log(posts)
+			} catch (error) {
+				console.error('Error fetching posts', error);
+			}
+			};
+			fetchPosts();
+	})
 return (
 	<div>
 		<div className='feed'>
-			<div className='post'>
+			{posts.map(post =>(
+				<div className='post'>
 				<div className='poster'>
 					<img className='profilePicture' src='/kirb.jpg' height={100} width={100} />
 					<div className='textInfo'>
-						<h1 className='username'>Kirby Watterson</h1>
-						<p className='postTime'>August 13, 2024</p>
+						<h1 className='username'>{post.userId}</h1>
+						<p className='postTime'>{post.timestamp}</p>
 					</div>
 				</div>
-				<p>This is the post that I, kirby, am making</p>
+				<p>{post.content}</p>
 			</div>
+			))}
 		</div>
 	</div>
 )
--- src/Pages/profile.jsx
+++ src/Pages/profile.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import './Profile.css'
+import { createPortal } from 'react-dom';
+import LoginForm from '../Components/LoginForm';
+import Cookies from 'js-cookie';
+import { jwtDecode } from 'jwt-decode';
 
 const Profile = () => {
+  const [validCookie, setValidCookie] = useState(Cookies.get('loginAuth'));
+  const [decryptToken, setDecryptToken] = useState(Cookies.get('loginAuth') ? jwtDecode(Cookies.get('loginAuth')) : undefined);
+  const [openModal, setOpenModal] = useState(Cookies.get('loginAuth') ? false : true)
+
+  const fetchLogin = async(e) => {
+    e.preventDefault();
+  
+    const username = e.target.username.value;
+    const password = e.target.password.value;
+  
+    try{
+      const response = await fetch('https://knule.duckdns.org/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+          "username": username,
+          "password": password,
+          
+        })
+    });
+  
+      const loginResult = await response.json();
+      const inFifteen = new Date(new Date().getTime() + 0.25 * 60 * 1000)
+      Cookies.set('loginAuth', loginResult.acessToken,
+        {
+          expires: inFifteen
+        }
+      );
+      const decoded = jwtDecode(loginResult.acessToken);
+      //console.log(decoded); 
+      setValidCookie(Cookies.get('loginAuth'))
+      
+    } catch (error) {
+      setValidCookie(undefined);
+      console.error('Error authenticating login', error);
+    }
+  }
+    
+
+  useEffect(() => {
+    const login = Cookies.get('loginAuth');
+      
+    setValidCookie(login ? login : undefined);
+    setDecryptToken(login ? jwtDecode(validCookie) : "Yeah this fucked up")
+    setOpenModal(login ? false : true)
+    console.log({validCookie})
+    //console.log({decryptToken})
+    //console.log({openModal})
+
+  }, [validCookie])
 return (
 	<div>
+    {openModal && createPortal(
+          <LoginForm onSubmit={fetchLogin} />,
+          document.body
+      )}
 	<div className='profile'>
 		<div className="images">
 		<img className= 'banner' src='/kirbBanner.jpg'/>
@@ -11,7 +71,7 @@ return (
 		</div>
 		
 		<div className='profile-text'>
-		<h1 className='username'>Kirby Watterson</h1>
+		<h1 className='username'>{validCookie ? decryptToken.username : "Kirby Watterson"}</h1>
 		<h3 className='fullName'>@kirbistheword</h3>
 		</div>
 		<p className='bio'>

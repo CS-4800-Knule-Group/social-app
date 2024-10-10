e8da7874e83c416fa881ee041feeaafebd864f27
Ecastaneda0422 <ecastaneda0422@gmail.com>
2024-10-10 14:11:12 -0700

    Modal

--- src/Pages/Messages.jsx
+++ src/Pages/Messages.jsx
@@ -1,8 +1,73 @@
-import React, { useState, useEffect } from 'react'
-import './Messages.css'
+import React, {useEffect, useState} from 'react'
 import { Link } from 'react-router-dom'
+import './Messages.css'
+import { createPortal } from 'react-dom';
+import LoginForm from '../Components/LoginForm.jsx';
+import Cookies from 'js-cookie';
+import { jwtDecode } from 'jwt-decode';
+
 
 const Messages = () => {
+
+  const [validCookie, setValidCookie] = useState(Cookies.get('loginAuth'));
+  const [decryptToken, setDecryptToken] = useState(Cookies.get('loginAuth') ? jwtDecode(Cookies.get('loginAuth')) : undefined);
+  const [openModal, setOpenModal] = useState(Cookies.get('loginAuth') ? false : true)
+  const [openChat, setOpenChat] = useState(false)
+
+  const fetchLogin = async(e) => {
+    e.preventDefault();
+  
+    const username = e.target.username.value;
+    const password = e.target.password.value;
+  
+    try{
+      const response = await fetch('https://knule.duckdns.org/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type' : 'application/json'
+        },
+        body: JSON.stringify({
+          "username": username,
+          "password": password,
+          
+        })
+    });
+  
+      const loginResult = await response.json();
+      const inFifteen = new Date(new Date().getTime() + 2 * 60 * 1000)
+      Cookies.set('loginAuth', loginResult.acessToken,
+        {
+          expires: inFifteen
+        }
+      );
+      const decoded = jwtDecode(loginResult.acessToken);
+      //console.log(decoded); 
+      setValidCookie(Cookies.get('loginAuth'))
+      
+    } catch (error) {
+      setValidCookie(undefined);
+      console.error('Error authenticating login', error);
+    }
+  }
+    
+
+  useEffect(() => {
+    const login = Cookies.get('loginAuth');
+      
+    setValidCookie(login ? login : undefined);
+    setDecryptToken(login ? jwtDecode(validCookie) : "Yeah this fucked up")
+    setOpenModal(login ? false : true)
+    console.log({validCookie})
+    console.log({decryptToken})
+    //console.log({openModal})
+
+  }, [validCookie])
+
+  const toggleChat = () => {
+    setOpenChat(openChat => !openChat)
+    console.log(openChat)
+  }
+
   // const apiChat = 'https://knule.duckdns.org/'
   
   
@@ -29,9 +94,17 @@ const Messages = () => {
   
   return (
     <div>
-        {/*turn into modal */}
-        <button>Start a new chat</button>
-    
+         {openModal && createPortal(
+          <LoginForm onSubmit={fetchLogin} />,
+          document.body
+      )}
+      {openChat && createPortal(
+          <newMsg/>,
+          document.body
+      )}
+        <button onClick={toggleChat}>Start a new chat</button>
+
+       
         
         <br/>
       <h1>Messages</h1>

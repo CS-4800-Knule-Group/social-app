6f1925db4b474d2ad0ae3a5ce162531136535c4e
Otwo06 <46037767+Otwo6@users.noreply.github.com>
2024-10-10 20:17:41 -0700

    Empty Username or Password Error

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,29 +1,65 @@
-import React, {useEffect} from 'react'
-import './LoginForm.css'
+import React, { useState } from 'react';
+import './LoginForm.css';
 
+const LoginForm = ({ onSubmit }) => {
+const [username, setUsername] = useState('');
+const [password, setPassword] = useState('');
+const [errors, setErrors] = useState({});
 
+const validate = () => {
+    const newErrors = {};
+    if (!username) {
+    newErrors.username = 'Username is required';
+    }
+    if (!password) {
+    newErrors.password = 'Password is required';
+    }
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+};
 
+const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validate()) {
+    onSubmit({ username, password });
+    }
+};
 
-const LoginForm = ({onSubmit}) => {
-  
-  return (
+return (
     <div className="login-form">
-        <div className="form-box solid">
-            <form onSubmit={onSubmit}>
-            <h1 className="login-text">Sign In</h1>
-            <label>Username</label>
-            <br></br>
-            <input type="text" name="username" className="login-box" />
-            <br></br>
-            <label>Password</label>
-            <br></br>
-            <input type="password" name="password" className="login-box" />
-            <br></br>
-            <input type="submit" value="LOGIN" className="login-btn" />
-            </form>
-        </div>
-    </div>
-  )
-}
+    <div className="form-box solid">
+        <form onSubmit={handleSubmit}>
+        <h1 className="login-text">Sign In</h1>
 
-export default LoginForm
+        <label>Username</label>
+        <br />
+        <input
+            type="text"
+            name="username"
+            className="login-box"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+        />
+        {errors.username && <p className="error">{errors.username}</p>}
+        <br />
+
+        <label>Password</label>
+        <br />
+        <input
+            type="password"
+            name="password"
+            className="login-box"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+        />
+        {errors.password && <p className="error">{errors.password}</p>}
+        <br />
+
+        <input type="submit" value="LOGIN" className="login-btn" />
+        </form>
+    </div>
+    </div>
+);
+};
+
+export default LoginForm;

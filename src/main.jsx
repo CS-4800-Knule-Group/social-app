7089c3e43806ea5c9a6d6f6e12b25fd969e9bc4c
Nathanael Garcia <nathanael.garcia@sjsu.edu>
2024-10-26 22:27:55 -0700

    Added env Files

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,15 @@ import MsgTemp from './Pages/MsgTemp.jsx'
 import ProfileOther from './Pages/ProfileOther.jsx'
 import Explore from './Pages/Explore.jsx'
 import { Amplify } from 'aws-amplify';
-import awsConfig from "./aws-exports.js";
+
+const awsmobile = {
+  "aws_project_region": "us-west-2",
+  "aws_appsync_graphqlEndpoint": process.env.appsync_graphqlEndpoint,
+  "aws_appsync_region": "us-west-2",
+  "aws_appsync_authenticationType": "API_KEY",
+  "aws_appsync_apiKey": process.env.appsync_apikey
+};
+
 Amplify.configure(awsConfig);
 
 

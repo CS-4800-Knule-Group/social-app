a5807a07df8e77d248caf7f4e3f6b0de08230baa
Otwo06 <46037767+Otwo6@users.noreply.github.com>
2024-10-27 22:38:06 -0700

    Show Posts on Profile Page

--- src/Pages/profile.jsx
+++ src/Pages/profile.jsx
@@ -6,132 +6,170 @@ import Cookies from 'js-cookie';
 import { jwtDecode } from 'jwt-decode';
 
 const Profile = () => {
-  
-  const apiUsers = 'https://knule.duckdns.org/users'
-  const [validCookie, setValidCookie] = useState(Cookies.get('loginAuth'));
-  const [decryptToken, setDecryptToken] = useState(Cookies.get('loginAuth') ? jwtDecode(Cookies.get('loginAuth')) : undefined);
-  const [openModal, setOpenModal] = useState(Cookies.get('loginAuth') ? false : true)
-  const [users, setUsers] = useState([])
 
-  //Compare user login and update cookies if valid
-  const fetchLogin = async(e) => {
-    e.preventDefault();
-  
-    const username = e.target.username.value;
-    const password = e.target.password.value;
-  
-    try{
-      const response = await fetch('https://knule.duckdns.org/auth/login', {
-        method: 'POST',
-        headers: {
-          'Content-Type' : 'application/json'
-        },
-        body: JSON.stringify({
-          "username": username,
-          "password": password,
-          
-        })
-    });
-  
-      const loginResult = await response.json();
-      const inFifteen = new Date(new Date().getTime() + 2 * 60 * 1000)
-      Cookies.set('loginAuth', loginResult.acessToken,
-        {
-          expires: inFifteen
-        }
-      );
-      const decoded = jwtDecode(loginResult.acessToken);
-      //console.log(decoded); 
-      setValidCookie(Cookies.get('loginAuth'))
-      
-    } catch (error) {
-      setValidCookie(undefined);
-      console.error('Error authenticating login', error);
-    }
-  }
-    
+const apiPosts = 'https://knule.duckdns.org/posts'
+const apiUsers = 'https://knule.duckdns.org/users'
+const [validCookie, setValidCookie] = useState(Cookies.get('loginAuth'));
+const [decryptToken, setDecryptToken] = useState(Cookies.get('loginAuth') ? jwtDecode(Cookies.get('loginAuth')) : undefined);
+const [openModal, setOpenModal] = useState(Cookies.get('loginAuth') ? false : true)
+const[posts, setPosts] = useState([])
+const [users, setUsers] = useState([])
 
-  //On validCookie change, update Decrypt Token and OpenModal
-  useEffect(() => {
-    const login = Cookies.get('loginAuth');
-      
-    setValidCookie(login ? login : undefined);
-    setDecryptToken(login ? jwtDecode(validCookie) : "Yeah this fucked up")
-    setOpenModal(login ? false : true)
-    console.log({validCookie})
-    console.log({decryptToken})
-    //console.log({openModal})
+const fetchLogin = async(e) => {
+	e.preventDefault();
 
-  }, [validCookie])
+	const username = e.target.username.value;
+	const password = e.target.password.value;
 
-  
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const response = await fetch(apiUsers, {
-          method: 'GET', // should be lowercase 'method'
-        });
+	try{
+	const response = await fetch('https://knule.duckdns.org/auth/login', {
+		method: 'POST',
+		headers: {
+		'Content-Type' : 'application/json'
+		},
+		body: JSON.stringify({
+		"username": username,
+		"password": password,
+		
+		})
+	});
 
-        if (!response.ok) {
-          throw new Error('Could not reach /users');
-        }
-        const usersData = await response.json();
-        setUsers(usersData);
-      } catch (error) {
-        console.error('Error fetching users', error);
-      }
+	const loginResult = await response.json();
+	const inFifteen = new Date(new Date().getTime() + 2 * 60 * 1000)
+	Cookies.set('loginAuth', loginResult.acessToken,
+		{
+		expires: inFifteen
+		}
+	);
+	const decoded = jwtDecode(loginResult.acessToken);
+	//console.log(decoded); 
+	setValidCookie(Cookies.get('loginAuth'))
+	
+	} catch (error) {
+	setValidCookie(undefined);
+	console.error('Error authenticating login', error);
+	}
+}
+	
 
-      
-    };
-    fetchUsers();
-  }, []);  // only re-run the effect if apiEndpoint changes
+useEffect(() => {
+	const login = Cookies.get('loginAuth');
+	
+	setValidCookie(login ? login : undefined);
+	setDecryptToken(login ? jwtDecode(validCookie) : "Yeah this fucked up")
+	setOpenModal(login ? false : true)
+	console.log({validCookie})
+	console.log({decryptToken})
+	//console.log({openModal})
 
-  if(decryptToken){
-    for(let i = 0; i < users.length; i++){
-      if(users[i].userId == decryptToken.userId){
-          setUsers(users[i]);
-          break;
-      }
-    };
-    console.log(users)
-  }
+}, [validCookie])
 
+useEffect(() => {
+	const fetchUsers = async () => {
+	try {
+		const response = await fetch(apiUsers, {
+		method: 'GET', // should be lowercase 'method'
+		});
 
+		if (!response.ok) {
+		throw new Error('Could not reach /users');
+		}
+		const usersData = await response.json();
+		setUsers(usersData);
+	} catch (error) {
+		console.error('Error fetching users', error);
+	}
+
+	
+	};
+	fetchUsers();
+}, []);  // only re-run the effect if apiEndpoint changes
+
+if(decryptToken){
+	for(let i = 0; i < users.length; i++){
+	if(users[i].userId == decryptToken.userId){
+		setUsers(users[i]);
+		break;
+	}
+	};
+	console.log(users)
+}
+
+useEffect(() => {
+		
+	const fetchPosts = async () => {
+		try {
+			const response = await fetch(apiPosts, {
+			method: 'GET', // should be lowercase 'method'
+			});
+	
+			if (!response.ok) {
+			throw new Error('Could not reach /posts');
+			}
+			const postsData = await response.json();
+
+			const userId = decryptToken.userId;
+
+			const filterPosts = postsData.filter(post => post.userId == userId);
+
+			console.log(postsData[0].timestamp);
+			setPosts(filterPosts.sort((x, y) => {
+				return new Date(y.timestamp) - new Date(x.timestamp);    
+			})); // Update the state with the fetched users
+			console.log(posts)
+		} catch (error) {
+			console.error('Error fetching posts', error);
+		}
+		};
+		fetchPosts();
+}, [])
 
 
 return (
 	<div>
-    {openModal && createPortal(
-          <LoginForm onSubmit={fetchLogin} />,
-          document.body
-      )}
-	<div className='profile'>
-		<div className="images">
-		<img className= 'banner' src='/kirbBanner.jpg'/>
-			<img className= 'profilePic' src='/kirb.jpg' height={100} width={100} />
-		</div>
-		
-		<div className='profile-text'>
-		<h1 className='username'>{validCookie ? decryptToken.username : "Kirby Watterson"}</h1>
-		<h3 className='fullName'>@kirbistheword</h3>
-		</div>
-		<p className='bio'>
-			I play video games and am the star of my own video game franchise. I'm not as popular as Mario and Sonic but at least I wasn't replaced with a robot like Sackboy.
-		</p>
-		<div className='follow-section'>
-			<div className='follow-text'>
-				<p className='followers-text'>{users.followers? users.followers.length : "unknown"}</p>
-				<p>Followers</p>
+	{openModal && createPortal(
+		<LoginForm onSubmit={fetchLogin} />,
+		document.body
+	)}
+		<div className='profile'>
+			<div className="images">
+			<img className= 'banner' src='/kirbBanner.jpg'/>
+				<img className= 'profilePic' src='/kirb.jpg' height={100} width={100} />
 			</div>
-			<div className='vertical-line'></div>
-			<div className='follow-text'>
-				<p className='following-text'>{users.following? users.following.length : "unknown"}</p>
-				<p>Following</p>
-			</div>
-			<div className='vertical-line'></div>
 			
+			<div className='profile-text'>
+			<h1 className='username'>{validCookie ? decryptToken.username : "Kirby Watterson"}</h1>
+			<h3 className='fullName'>@kirbistheword</h3>
+			</div>
+			<p className='bio'>
+				I play video games and am the star of my own video game franchise. I'm not as popular as Mario and Sonic but at least I wasn't replaced with a robot like Sackboy.
+			</p>
+			<div className='follow-section'>
+				<div className='follow-text'>
+					<p className='followers-text'>{users.followers? users.followers.length : "unknown"}</p>
+					<p>Followers</p>
+				</div>
+				<div className='vertical-line'></div>
+				<div className='follow-text'>
+					<p className='following-text'>{users.following? users.following.length : "unknown"}</p>
+					<p>Following</p>
+				</div>
+				<div className='vertical-line'></div>
+			</div>
+			{posts.map(post =>(
+				<div className='post'>
+					<div className='poster'>
+						<img className= 'post-profilePic' src='/kirb.jpg' height={100} width={100} />
+						<h1 className='post-username'>{validCookie ? decryptToken.username : "Kirby Watterson"}</h1>
+						<h3 className='post-fullName'>@kirbistheword</h3>
+						<div className='textInfo'>
+							<p className='postTime'>{post.timestamp}</p>
+						</div>
+					</div>
+					<p>{post.content}</p>
+				</div>
+			))}
 		</div>
-	</div>
 	</div>
 )
 }

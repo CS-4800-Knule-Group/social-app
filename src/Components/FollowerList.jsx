218f260e099c0a7dcacc812d4697c834adc86ab9
Otwo06 <46037767+Otwo6@users.noreply.github.com>
2024-11-27 07:22:24 -0800

    Follower and Following List show user pfp

--- src/Components/FollowerList.jsx
+++ src/Components/FollowerList.jsx
@@ -4,13 +4,18 @@ import './LoginForm.css';
 
 const FollowerList = ({ followers, onClose }) => {
 
+	const getProfilePicture = (userId) => {
+        const user = followers.find(u => u.userId === userId)
+        return user ? user.pfp : '/defaultProfilePic.jpg' // Fallback profile picture
+    }
+
 	return (
 		<div className="login-form">
 			<p onClick={onClose}>X</p>
 			{followers.map(followers => (
 				<div key={followers.userId} className='follower'>
 					<Link to={'/profile/' + followers.userId}>
-						<img className='follower-profilePic' src='/kirb.jpg' height={100} width={100} />
+						<img className='follower-profilePic' src={getProfilePicture(followers.userId)} />
 						<h1 className='follower-username'>{followers.username}</h1>
 					</Link>
 				</div>))}
--- src/Components/FollowingList.jsx
+++ src/Components/FollowingList.jsx
@@ -3,6 +3,10 @@ import { Link } from 'react-router-dom'
 import './LoginForm.css';
 
 const FollowingList = ({ following, onClose }) => {
+    const getProfilePicture = (userId) => {
+        const user = following.find(u => u.userId === userId)
+        return user ? user.pfp : '/defaultProfilePic.jpg' // Fallback profile picture
+    }
 
     return (
         <div className="login-form">
@@ -10,7 +14,7 @@ const FollowingList = ({ following, onClose }) => {
             {following.map(following => (
                 <div key={following.userId} className='following'>
                     <Link to={'/profile/' + following.userId}>
-                        <img className='following-profilePic' src='/kirb.jpg' height={100} width={100} />
+                        <img className='following-profilePic' src={getProfilePicture(following.userId)} />
                         <h1 className='following-username'>{following.username}</h1>
                     </Link>
                 </div>)

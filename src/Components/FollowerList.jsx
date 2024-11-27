164f756ce5ba05580a1f1b5d392d50f3bc523b7a
Ecastaneda0422 <ecastaneda0422@gmail.com>
2024-11-27 11:17:45 -0800

    hotfix
    fixed linking to follower page on click and register page header margin

--- src/Components/FollowerList.jsx
+++ src/Components/FollowerList.jsx
@@ -1,26 +1,32 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 import './LoginForm.css';
 
 const FollowerList = ({ followers, onClose }) => {
+    const navigate = useNavigate();
 
-	const getProfilePicture = (userId) => {
+    const getProfilePicture = (userId) => {
         const user = followers.find(u => u.userId === userId)
         return user ? user.pfp : '/defaultProfilePic.jpg' // Fallback profile picture
-    }
+    };
 
-	return (
-		<div className="login-form">
-			<p onClick={onClose}>X</p>
-			{followers.map(followers => (
-				<div key={followers.userId} className='follower'>
-					<Link to={'/profile/' + followers.userId}>
-						<img className='follower-profilePic' src={getProfilePicture(followers.userId)} />
-						<h1 className='follower-username'>{followers.username}</h1>
-					</Link>
-				</div>))}
-		</div>
-	);
+    const openPage = (userId) => {
+        navigate(`/profile/${userId}`);
+        location.reload();
+    };
+
+    return (
+        <div className="login-form">
+            <p onClick={onClose}>X</p>
+            {followers.map(followers => (
+                <div key={followers.userId} className='follower'>
+                    <div onClick={() => openPage(followers.userId)}>
+                        <img className='follower-profilePic' src={getProfilePicture(followers.userId)} />
+                        <h1 className='follower-username'>{followers.username}</h1>
+                    </div>
+                </div>))}
+        </div>
+    );
 };
 
 export default FollowerList;
--- src/Components/FollowingList.jsx
+++ src/Components/FollowingList.jsx
@@ -1,25 +1,31 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './LoginForm.css';
 
 const FollowingList = ({ following, onClose }) => {
+    const navigate = useNavigate();
+
     const getProfilePicture = (userId) => {
-        const user = following.find(u => u.userId === userId)
-        return user ? user.pfp : '/defaultProfilePic.jpg' // Fallback profile picture
-    }
+        const user = following.find(u => u.userId === userId);
+        return user ? user.pfp : '/defaultProfilePic.jpg'; // Fallback profile picture
+    };
+
+    const openPage = (userId) => {
+        navigate(`/profile/${userId}`);
+        location.reload();
+    };
 
     return (
         <div className="login-form">
             <p onClick={onClose}>X</p>
             {following.map(following => (
                 <div key={following.userId} className='following'>
-                    <Link to={'/profile/' + following.userId}>
-                        <img className='following-profilePic' src={getProfilePicture(following.userId)} />
+                    <div onClick={() => openPage(following.userId)}>
+                        <img className='following-profilePic' src={getProfilePicture(following.userId)} alt={following.username} />
                         <h1 className='following-username'>{following.username}</h1>
-                    </Link>
-                </div>)
-            )
-            }
+                    </div>
+                </div>
+            ))}
         </div>
     );
 };

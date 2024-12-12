0d176ff22dc242b2fb970b7feefcdb82d95c226c
Otwo06 <46037767+Otwo6@users.noreply.github.com>
2024-12-12 12:01:27 -0800

    Msg CSS work

--- src/Pages/MsgTemp.jsx
+++ src/Pages/MsgTemp.jsx
@@ -199,28 +199,30 @@ const MsgTemp = () => {
                 </div>
                 <div className='MessagingScreen'>
                     <div className='Messaging'>
-                        <img className='profilePicture' src={tarUser.length != 0 ? tarUser[0].pfp != undefined ? tarUser[0].pfp : '/kirb.jpg' : '/kirb.jpg'} height={100} width={100} />
-                        <h1>{tarUser.length != 0 ? tarUser[0].username : "No user found"}</h1>
+                        <div className='poster'>
+                            <img className='profilePicture' src={tarUser.length != 0 ? tarUser[0].pfp != undefined ? tarUser[0].pfp : '/kirb.jpg' : '/kirb.jpg'} height={100} width={100} />
+                            <h1>{tarUser.length != 0 ? tarUser[0].username : "No user found"}</h1>
+                        </div>
                         <div id='chat'>
-                        {chats
-                        .sort((a, b) => a.createdAt.localeCompare(b.createdAt))
-                        .map((chat) => (
-                            <div
-                            key={chat.id}
-                            className="chats"
-                            >
-                                <div>
-                                    <p>{chat.userId == user.userId ? "You: " + chat.text : 
-                                    tarUser.length ? tarUser[0].username + ": " + chat.text : "User not found"}</p>
+                            {chats
+                            .sort((a, b) => a.createdAt.localeCompare(b.createdAt))
+                            .map((chat) => (
+                                <div
+                                key={chat.id}
+                                className="chats"
+                                >
+                                    <div>
+                                        <p>{chat.userId == user.userId ? "You: " + chat.text : 
+                                        tarUser.length ? tarUser[0].username + ": " + chat.text : "User not found"}</p>
+                                    </div>
                                 </div>
-                            </div>
-                        ))}
+                            ))}
                         </div>
                     </div>
                     <div className='MessagingTexter'>
-                        <input type='text' id='msgInput' placeholder='Type your msg here...' onChange={e => setNewMessage(e.target.value)}
+                        <input className='MessageChat' type='text' id='msgInput' placeholder='Type your msg here...' onChange={e => setNewMessage(e.target.value)}
                         value={newMessage} onKeyUp={sendMessageEnter}/>
-                        <button onClick={sendMessage}>Send</button>
+                        <button className='MessageSend' onClick={sendMessage}>➤</button>
                     </div>
                 </div>
             </div>
